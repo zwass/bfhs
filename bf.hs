690e669b43b16b20b7602942f0514e1d2f9fedcf
Zachary Wasserman <zachwass2000@gmail.com>
2013-05-30 21:21:51 -0700

    Working correctly for many sample programs

--- bf.hs
+++ bf.hs
@@ -1,36 +1,54 @@
+import Prelude hiding (catch)
 import Data.Array
+import System.Environment (getArgs)
+import System.IO
+import System.IO.Error (isEOFError)
+import Control.Exception (catch)
 
 type Memory = Array Int
 
 data BFState = BFState { dataMemory :: Memory Int
                        , dataPointer :: Int
                        , insnMemory :: Memory Char
-                       , insnPointer :: Int
-                       , input :: String
-                       , output :: String }
+                       , insnPointer :: Int }
              deriving (Show)
 
 memMax :: Int
-memMax = 29999
+memMax = 30000
 
 initialDataMemory :: Memory Int
 initialDataMemory = listArray (0, memMax) (repeat 0)
 
 initialInsnMemory :: Memory Char
-initialInsnMemory = listArray (0, memMax) $ "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." ++ repeat ' '
+initialInsnMemory = mkInsnMemory ">,[>,]<[.<]"
+
+mkInsnMemory :: String -> Memory Char
+mkInsnMemory s = listArray (0, memMax) $ s ++ repeat ' '
 
 initialBFState :: BFState
-initialBFState = BFState initialDataMemory 0 initialInsnMemory 0 "" ""
+initialBFState = BFState initialDataMemory 0 initialInsnMemory 0
 
 jumpToMatchingCloseBrace :: BFState -> BFState
-jumpToMatchingCloseBrace s = case insnMemory s ! insnPointer s of
-  ']' -> s
-  _   -> jumpToMatchingCloseBrace $ s {insnPointer = insnPointer s + 1}
+jumpToMatchingCloseBrace = jumpToMatchingCloseBrace' (-1) where
+  jumpToMatchingCloseBrace' n s = case insnMemory s ! insnPointer s of
+    ']' | n == 0 -> s
+    ']' -> jumpToMatchingCloseBrace' (n - 1) $ s'
+    '[' -> jumpToMatchingCloseBrace' (n + 1) $ s'
+    _   -> jumpToMatchingCloseBrace' n $ s'
+    where s' = s {insnPointer = insnPointer s + 1}
 
 jumpToMatchingOpenBrace :: BFState -> BFState
-jumpToMatchingOpenBrace s = case insnMemory s ! insnPointer s of
-  '[' -> s
-  _   -> jumpToMatchingOpenBrace $ s {insnPointer = insnPointer s - 1}
+jumpToMatchingOpenBrace = jumpToMatchingOpenBrace' (-1) where
+  jumpToMatchingOpenBrace' n s = case insnMemory s ! insnPointer s of
+    '[' | n == 0 -> s
+    '[' -> jumpToMatchingOpenBrace' (n - 1) $ s'
+    ']' -> jumpToMatchingOpenBrace' (n + 1) $ s'
+    _   -> jumpToMatchingOpenBrace' n $ s'
+    where s' = s {insnPointer = insnPointer s - 1}
+
+getChar' :: IO Char
+getChar' =  getChar `catch` eofHandler where
+  eofHandler e = if isEOFError e then return '\0' else ioError e
 
 evaluate :: BFState -> IO BFState
 evaluate s | insnPointer s > memMax = return s
@@ -41,7 +59,7 @@ evaluate s =
         '+' -> return $ s {dataMemory = dMem // [(dPtr, dVal + 1)]}
         '-' -> return $ s {dataMemory = dMem // [(dPtr, dVal - 1)]}
         '.' -> putStr [toEnum dVal] >> return s
-        ',' -> do c <- getChar
+        ',' -> do c <- getChar'
                   return $ s {dataMemory = dMem // [(dPtr, fromEnum c)] }
         '[' | dVal == 0 -> return $ jumpToMatchingCloseBrace s
         ']' | dVal /= 0 -> return $ jumpToMatchingOpenBrace s
@@ -53,3 +71,15 @@ evaluate s =
         dVal = dMem ! dPtr
         iMem = insnMemory s
         iPtr = insnPointer s
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  args <- getArgs
+  if length args /= 1
+    then putStrLn "No input program provided. Exiting."
+    else do
+    code <- readFile $ head args
+    let initState = initialBFState {insnMemory = mkInsnMemory code}
+    _ <- evaluate initState
+    return ()
